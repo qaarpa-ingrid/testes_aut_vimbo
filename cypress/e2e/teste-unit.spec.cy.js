e523b1f455d0c4856f999b00349abfde9bc52393
qaarpa-ingrid <qaarpa@gmail.com>
2026-03-03 17:32:23 -0300

    menu_negociacao

---
+++ cypress/e2e/teste-unit.spec.cy.js
@@ -0,0 +1,32 @@
+describe('teste aut', () => {
+  it('passes', () => {
+    cy.visit('https://app-homologacao.vimbo.com.br/')
+
+    // acessar com conta master vimbo 
+    cy.get('input[formControlName="usu_email"]')
+    .type('sistema@vimbo.com.br')
+    cy.get('input[formControlName="usu_senha"]')
+    .type('Arp@!135')
+    cy.get('button[aria-label="LOGIN"]')
+    .click()
+
+    // clicar no menu
+    cy.get('mat-icon[svgicon="vb-icon-menu-open"]')
+  .closest('button')
+  .click()
+
+// abrir o menu de vendas e negociações 
+  cy.contains('span.nav-link-title', 'Vendas')
+  .click()
+  cy.contains('a.nav-link', 'Negociações')
+  .should('be.visible')
+  .click()
+
+
+
+
+
+
+
+  })
+})
